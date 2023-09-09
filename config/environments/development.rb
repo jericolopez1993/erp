b818a8e502bf13228263218f6fd0f4bd43ef9676
Jerico Lopez <jerico.lopez1993@gmail.com>
2023-09-09 21:48:12 +0800

    add active storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
 
   config.assets.compile = true
 
+  config.active_storage.service = :local
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
